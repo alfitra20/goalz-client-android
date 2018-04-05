cb0feee3f43781f6b0e635ffad5a3b42e19e5237
alfitra20 <alfitrarahman@gmail.com>
2018-04-05 16:20:10 +0300

    Create the sidebar and activities related

---
+++ app/src/main/java/emse/mobisocial/goalz/UsersLibraryActivity.kt
@@ -0,0 +1,23 @@
+package emse.mobisocial.goalz
+
+import android.os.Bundle
+import android.widget.FrameLayout
+import kotlinx.android.synthetic.main.activity_base_basic.*
+
+class UsersLibraryActivity : BaseActivity() {
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_base_basic)
+        setSupportActionBar(toolbar_basic)
+        val actionbar = supportActionBar
+        actionbar?.title = getString(R.string.app_bar_users_library)
+
+        //Initialize the Navigation view and menu + floating button
+        setFabBasic()
+
+        val contentFrameLayout = content_frame as FrameLayout
+        layoutInflater.inflate(R.layout.activity_users_library, contentFrameLayout)
+    }
+
+}
