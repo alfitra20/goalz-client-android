85f93235881cc1e6b2fc4184da31cbfcdc45f775
alfitra20 <alfitrarahman@gmail.com>
2018-04-16 13:23:59 +0300

    change goal activity to be a tabbed activity

--- app/src/main/java/emse/mobisocial/goalz/ui/GoalActivity.kt
+++ app/src/main/java/emse/mobisocial/goalz/ui/GoalActivity.kt
@@ -1,23 +1,71 @@
 package emse.mobisocial.goalz.ui
 
+import android.support.v4.app.Fragment
+import android.support.v4.app.FragmentManager
+import android.support.v4.app.FragmentPagerAdapter
 import android.os.Bundle
-import android.widget.FrameLayout
+import android.view.Menu
+import android.view.MenuItem
+
 import emse.mobisocial.goalz.R
-import kotlinx.android.synthetic.main.activity_base_basic.*
+import emse.mobisocial.goalz.test.fragments.TestGoalFragment
+import emse.mobisocial.goalz.test.fragments.TestRecommendationFragment
+import emse.mobisocial.goalz.test.fragments.TestResourceFragment
+import kotlinx.android.synthetic.main.activity_base.*
 
 class GoalActivity : BaseActivity() {
 
+    private var mSectionsPagerAdapter: SectionsPagerAdapter? = null
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_base_basic)
-        setSupportActionBar(toolbar_basic)
-        val actionbar = supportActionBar
-        actionbar?.title = "Goal Title"
 
         //Initialize the Navigation view and menu + floating button
-        setFabBasic()
+        setFab()
 
-        val contentFrameLayout = content_frame as FrameLayout
-        layoutInflater.inflate(R.layout.activity_goal, contentFrameLayout)
+        val actionbar = supportActionBar
+        //Put the correct goal title to the bar
+        actionbar?.title = "Goal 1"
+
+        mSectionsPagerAdapter = SectionsPagerAdapter(supportFragmentManager)
+
+        container.adapter = mSectionsPagerAdapter
+
+    }
+
+
+    override fun onCreateOptionsMenu(menu: Menu): Boolean {
+
+        menuInflater.inflate(R.menu.menu_goal, menu)
+        return true
+    }
+
+    override fun onOptionsItemSelected(item: MenuItem): Boolean {
+
+        val id = item.itemId
+
+        if (id == R.id.action_settings) {
+            return true
+        }
+
+        return super.onOptionsItemSelected(item)
+    }
+
+
+    inner class SectionsPagerAdapter(fm: FragmentManager) : FragmentPagerAdapter(fm) {
+
+        override fun getItem(position: Int): Fragment {
+            when (position) {
+
+                0 -> return TestGoalFragment()
+                1 -> return TestRecommendationFragment()
+            }
+            return TestResourceFragment()
+        }
+
+        override fun getCount(): Int {
+            // Show 2 total pages.
+            return 2
+        }
     }
 }
