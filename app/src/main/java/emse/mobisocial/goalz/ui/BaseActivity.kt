47398d30a6fabafd6eec7231615342c7c0378ef9
alfitra20 <alfitrarahman@gmail.com>
2018-04-17 23:00:23 +0300

    Change the navigation menu to use fragments

--- app/src/main/java/emse/mobisocial/goalz/ui/BaseActivity.kt
+++ app/src/main/java/emse/mobisocial/goalz/ui/BaseActivity.kt
@@ -1,9 +1,10 @@
 package emse.mobisocial.goalz.ui
 
+import android.content.Context
 import android.content.Intent
+import android.content.res.Configuration
 import android.os.Bundle
-import android.support.design.widget.Snackbar
-import android.support.design.widget.NavigationView
+import android.support.v4.app.Fragment
 import android.support.v4.view.GravityCompat
 import android.support.v7.app.ActionBarDrawerToggle
 import android.support.v7.app.AppCompatActivity
@@ -13,41 +14,35 @@ import android.widget.ImageView
 import android.widget.TextView
 import emse.mobisocial.goalz.R
 import kotlinx.android.synthetic.main.activity_base.*
-import kotlinx.android.synthetic.main.activity_base_basic.*
 
 
-open class BaseActivity : AppCompatActivity(), NavigationView.OnNavigationItemSelectedListener {
+open class BaseActivity : AppCompatActivity() {
 
     // Temporary for deciding which navigation menu and header to be shown
     // If the user use the app without login
     // it will use without_login_base_drawer menu
     private var loggedIn =  true
+    private lateinit var mContext:Context
+    private lateinit var toggle: ActionBarDrawerToggle
+    lateinit var fragment: Fragment
+    lateinit var fragmentClass:Class<*>
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_base)
         setSupportActionBar(toolbar)
+        mContext = this@BaseActivity
     }
 
-    //Initialize the Navigation view and menu + floating button for Tabbed activity
-    fun setFab(){
-        fab.setOnClickListener { view ->
-            Snackbar.make(view, "Replace with your own action", Snackbar.LENGTH_LONG)
-                    .setAction("Action", null).show()
-        }
+    private fun setUpNav() {
 
-        // when the user using the app without login
         if(!loggedIn){
             nav_view.menu.clear()
             nav_view.inflateMenu(R.menu.without_login_base_drawer)
         }
-
-        val toggle = ActionBarDrawerToggle(
+        toggle = ActionBarDrawerToggle(
                 this, drawer_layout, toolbar, R.string.navigation_drawer_open, R.string.navigation_drawer_close)
-        drawer_layout.addDrawerListener(toggle)
-        toggle.syncState()
-
-        nav_view.setNavigationItemSelectedListener(this)
+        drawer_layout?.addDrawerListener(toggle)
 
         //Initialize profile menu from navigation view in Tabbed activity
         var header = nav_view.getHeaderView(0)
@@ -68,54 +63,66 @@ open class BaseActivity : AppCompatActivity(), NavigationView.OnNavigationItemSe
         }else{
             sidebarNickname.text = getString(R.string.app_name)
         }
+
+        nav_view.setNavigationItemSelectedListener { item ->
+            when (item.itemId) {
+
+            // Available for user who use the app without login
+                R.id.nav_explore -> {
+                    fragmentClass = ExploreFragment::class.java
+                }
+
+            // Available only for user who login
+                R.id.nav_goals -> {
+                    fragmentClass = GoalsFragment::class.java
+                }
+                R.id.nav_library -> {
+                    fragmentClass = UsersLibraryFragment::class.java
+                }
+                R.id.nav_timeline -> {
+
+                }
+                R.id.nav_setting -> {
+
+                }
+                R.id.nav_logout -> {
+
+                }
+
+            // Without login menu
+                R.id.nav_login -> {
+
+                }
+                R.id.nav_signup -> {
+
+                }
+            }
+            item.setChecked(true)
+            fragment = fragmentClass.newInstance() as Fragment
+            val fragmentManager = supportFragmentManager
+            fragmentManager.beginTransaction().replace(R.id.content_frame, fragment).commit()
+            drawer_layout.closeDrawer(GravityCompat.START)
+
+            true
+        }
+        toggle.syncState()
     }
 
-    //Initialize the Navigation view and menu + floating button for Normal activity
-    fun setFabBasic(){
-        fab_basic.setOnClickListener { view ->
-            Snackbar.make(view, "Replace with your own action", Snackbar.LENGTH_LONG)
-                    .setAction("Action", null).show()
-        }
+    override fun onPostCreate(savedInstanceState: Bundle?) {
+        super.onPostCreate(savedInstanceState)
+        setUpNav()
 
-        // when the user using the app without login
-        if(!loggedIn){
-            nav_view_basic.menu.clear()
-            nav_view_basic.inflateMenu(R.menu.without_login_base_drawer)
-        }
-
-        val toggle = ActionBarDrawerToggle(
-                this, drawer_layout_basic, toolbar_basic, R.string.navigation_drawer_open, R.string.navigation_drawer_close)
-        drawer_layout_basic.addDrawerListener(toggle)
         toggle.syncState()
+    }
 
-        nav_view_basic.setNavigationItemSelectedListener(this)
-
-        //Initialize profile menu from navigation view in Tabbed activity
-        var header_basic = nav_view_basic.getHeaderView(0)
-        var sidebarNickname : TextView = header_basic.findViewById(R.id.sidebar_nickname)
-        var profileImage : ImageView = header_basic.findViewById(R.id.profile_image)
-
-        // When the user logged in the profile picture and nickname will redirect to user's profile
-        // if user using the app without login, it will only show Goalz there instead of nickname (as for now)
-        if(loggedIn){
-            sidebarNickname.setOnClickListener {
-                val intent = Intent(this, UserActivity::class.java)
-                startActivity(intent)
-            }
-            profileImage.setOnClickListener {
-                val intent = Intent(this, UserActivity::class.java)
-                startActivity(intent)
-            }
-        }else{
-            sidebarNickname.text = getString(R.string.app_name)
-        }
+    override fun onConfigurationChanged(newConfig: Configuration?) {
+        super.onConfigurationChanged(newConfig)
+        toggle.onConfigurationChanged(newConfig)
     }
 
     override fun onBackPressed() {
         if (drawer_layout != null && drawer_layout.isDrawerOpen(GravityCompat.START)) {
             drawer_layout.closeDrawer(GravityCompat.START)
-        } else if (drawer_layout_basic != null && drawer_layout_basic.isDrawerOpen(GravityCompat.START)){
-            drawer_layout_basic.closeDrawer(GravityCompat.START)
         }else{
             super.onBackPressed()
         }
@@ -135,50 +142,5 @@ open class BaseActivity : AppCompatActivity(), NavigationView.OnNavigationItemSe
             R.id.action_settings -> return true
             else -> return super.onOptionsItemSelected(item)
         }
-    }
-
-    override fun onNavigationItemSelected(item: MenuItem): Boolean {
-        // Handle navigation view item clicks here.
-        when (item.itemId) {
-
-            // Available for user who use the app without login
-            R.id.nav_explore -> {
-                val intent = Intent(this, ExploreActivity::class.java)
-                startActivity(intent)
-            }
-
-            // Available only for user who login
-            R.id.nav_goals -> {
-                val intent = Intent(this, GoalsActivity::class.java)
-                startActivity(intent)
-            }
-            R.id.nav_library -> {
-                val intent = Intent(this, UsersLibraryActivity::class.java)
-                startActivity(intent)
-            }
-            R.id.nav_timeline -> {
-
-            }
-            R.id.nav_setting -> {
-
-            }
-            R.id.nav_logout -> {
-
-            }
-
-            // Without login menu
-            R.id.nav_login -> {
-
-            }
-            R.id.nav_signup -> {
-
-            }
-        }
-        if (drawer_layout != null) {
-            drawer_layout.closeDrawer(GravityCompat.START)
-        }else{
-            drawer_layout_basic.closeDrawer(GravityCompat.START)
-        }
-        return true
     }
 }
---
+++ app/src/main/java/emse/mobisocial/goalz/ui/UsersLibraryFragment.kt
@@ -0,0 +1,39 @@
+package emse.mobisocial.goalz.ui
+
+import android.content.Context
+import android.net.Uri
+import android.os.Bundle
+import android.support.v4.app.Fragment
+import android.view.LayoutInflater
+import android.view.View
+import android.view.ViewGroup
+
+import emse.mobisocial.goalz.R
+import kotlinx.android.synthetic.main.activity_base.view.*
+
+
+class UsersLibraryFragment : Fragment() {
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+
+    }
+
+    override fun onCreateView(inflater: LayoutInflater, container: ViewGroup?,
+                              savedInstanceState: Bundle?): View? {
+        // Inflate the layout for this fragment
+        val view =  inflater.inflate(R.layout.fragment_users_library, container, false)
+        val actionbar = view.toolbar
+        actionbar?.title = getString(R.string.app_bar_users_library)
+        return view
+    }
+
+    override fun onAttach(context: Context) {
+        super.onAttach(context)
+
+    }
+
+    override fun onDetach() {
+        super.onDetach()
+    }
+}
