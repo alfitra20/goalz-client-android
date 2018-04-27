dd4e4dc8bcad31af5ad63aa2b6f5c90aeb460e96
alfitra20 <alfitrarahman@gmail.com>
2018-04-27 12:33:21 +0300

    add network checking in Onboarding activity

--- app/src/main/java/emse/mobisocial/goalz/ui/OnboardingActivity.kt
+++ app/src/main/java/emse/mobisocial/goalz/ui/OnboardingActivity.kt
@@ -1,17 +1,22 @@
 package emse.mobisocial.goalz.ui
 
+import android.content.Context
 import android.content.Intent
+import android.net.ConnectivityManager
 import android.support.v7.app.AppCompatActivity
 import android.os.Bundle
 import android.preference.PreferenceManager
+import android.support.design.widget.Snackbar
 import android.view.WindowManager
 import com.google.firebase.auth.FirebaseAuth
 import emse.mobisocial.goalz.R
+import kotlinx.android.synthetic.main.activity_base.*
 import kotlinx.android.synthetic.main.activity_onboarding.*
 
 class OnboardingActivity : AppCompatActivity() {
 
-    private lateinit var mFirebaseAuth : FirebaseAuth
+    private var networkInfo:Boolean = false
+    private lateinit var mSnackbar: Snackbar
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
@@ -29,6 +34,18 @@ class OnboardingActivity : AppCompatActivity() {
         supportActionBar?.hide()
         window.addFlags(WindowManager.LayoutParams.FLAG_TRANSLUCENT_STATUS)
 
+        networkInfo = isNetworkAvailable()
+
+        mSnackbar = Snackbar.make(onboarding_layout,
+                "You are not connected to the Internet", Snackbar.LENGTH_LONG)
+        if (!networkInfo){
+            mSnackbar.show()
+            login_button.isEnabled = false
+            signup_button.isEnabled = false
+            without_login_button.isEnabled = false
+        }
+
+
         login_button.setOnClickListener {
             val intent = Intent(this, LoginActivity::class.java)
             startActivity(intent)
@@ -44,5 +61,10 @@ class OnboardingActivity : AppCompatActivity() {
             intent.flags = Intent.FLAG_ACTIVITY_NEW_TASK or Intent.FLAG_ACTIVITY_CLEAR_TASK
             startActivity(intent)
         }
+    }
+    private fun isNetworkAvailable():Boolean {
+        val connectivityManager = getSystemService(Context.CONNECTIVITY_SERVICE) as ConnectivityManager
+        val activeNetworkInfo = connectivityManager.activeNetworkInfo
+        return activeNetworkInfo != null && activeNetworkInfo.isConnected
     }
 }
